a425561f35976757c5b79dccf359e6f71e756fb9
Ozgur Dincer <ozgurdincer@kentajans.com>
2021-07-07 14:13:37 +0300

    underconstruction page

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+    <title>Pİya Ahşap Mobilya Dekorasyon</title>
+</svelte:head>
+<div>
+    <img src="piya-ahsap-logo-new.svg" alt="Piya Ahşap Mobilya Dekorasyon">
+    <h1>Piya Ahşap Mobilya Dekorasyon</h1>
+    <p>Site Mah. Samanyolu Cad. Deha Sok. No: 5/1</p>
+    <p>Ümraniye - İstanbul</p>
+    <p>Tel: 0216 534 0750</p>
+    <p>Email: info@piyaahsap.com</p>
+    <p><em>...yenileniyoruz...</em></p>
+</div>
+<style>
+    div {text-align: center; padding-top: 4em;}
+</style>
