b423f6e6bb4b8aa570dcc7fb2141b8ec6708c4dd
oratoran <oratoran@gmail.com>
2022-03-26 00:49:26 +0300

    logo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,12 +5,12 @@
 <div class="grid place-items-center h-screen text-center ">
     <div class="shadow-lg rounded-full flex items-center hover:shadow-sm">
         <a href="https://www.instagram.com/piyaahsap/">
-            <img class="my-auto w-[313px] block" src="piya-ahsap-logo-new.svg" alt="Piya Ahşap Mobilya Dekorasyon">
+            <img class="my-auto w-[200px] block" src="piya-ahsap-logo.svg" alt="Piya Ahşap Mobilya Dekorasyon">
         </a>
     </div>
     
     <div>
-        <h1 class="text-3xl my-2">Piya Ahşap Mobilya Dekorasyon</h1>
+        <h1 class="text-2xl my-2">Piya Ahşap Mobilya Dekorasyon</h1>
         <p>Site Mah. Samanyolu Cad. Deha Sok. No: 5/1</p>
         <p>Ümraniye - İstanbul</p>
     </div>
